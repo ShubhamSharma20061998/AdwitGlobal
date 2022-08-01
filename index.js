5089be08e3b44f09617e574ee20092b627ded88e
Shubham <shubhamjsp51@gmail.com>
2022-08-01 11:40:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $("#example").DataTable({
+    bFilter: false,
+    bInfo: false,
+    bLengthChange: false,
+    ordering: false,
+    bPaginate: false,
+  });
+});
+// "bPaginate": false,
+//     "bLengthChange": false,
+//     "bFilter": true,
+//     "bInfo": false,
+//     "bAutoWidth": false
